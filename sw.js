68cb37b673a47eceba2acb6d33f638b1ee9ae900
Mfumo wa kuweka matokeo <davidmalosha1@gmail.com>
2025-04-24 16:23:04 +0300

    Create sw.js
    Added sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'kamhanga-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
